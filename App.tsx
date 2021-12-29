b08c462b236aa9f371caa0cf2e1cb8fd04b5f0af
efique <36161285+efique@users.noreply.github.com>
2021-12-29 01:06:40 +0100

    Revert "firebase"

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { writeUserData } from './utils/firebase.js'
-import { signInWithFacebook } from './utils/auth.js';
-import Routes from './navigation'
 
 export default function App() {
   return (
-    <Routes />
-    //   <View style={styles.container}>
-    //     <Text onPress={() => {
-    // writeUserData('100', 'test')
-    //       signInWithFacebook()
-    //     }}>Open up App.tsx to start working on your app!</Text>
-    //     <Text></Text>
-    //     <StatusBar style="auto" />
-    //   </View >
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
