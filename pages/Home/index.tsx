699ecb1791c9944683c55122e07f90e52486efff
DJesusCry <lorenzo_98@hotmail.fr>
2021-12-31 03:02:29 +0100

    requete clip

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -59,6 +59,36 @@ export default function HomePage() {
         }
     ])
 
+    fetch('https://api.twitch.tv/helix/games/top?first=100', { // requete du top 100 des jeux twitch
+        method : 'GET',
+        headers : {
+            'Authorization' : 'Bearer jyjuqv7imq62o6gldfufqoqz4hklhr', 
+            'Client-id' : 'zq9uuf6vb5lfmuhrvcu9vnodpq91pv'
+        }
+    })
+    .then((response) => response.json()) // liste des jeux
+    .then((json) => {
+        console.log(json.data)
+        json.data.forEach((element : any) => { // Pour chaque jeu
+            //console.log(element.id +" : "+ element.name) 
+            fetch('https://api.twitch.tv/helix/clips?game_id=' + element.id  + '&first=10', { // requete des clips pour chaque jeux
+                method : 'GET',
+                headers : {
+                    'Authorization' : 'Bearer jyjuqv7imq62o6gldfufqoqz4hklhr',
+                    'Client-id' : 'zq9uuf6vb5lfmuhrvcu9vnodpq91pv'
+                }
+            })
+            .then((response) => response.json()) // liste des clips
+            .then((json) => {
+                console.log("\n"+element.name)
+                json.data.forEach((element : any) => { // pour chaque clip
+                    console.log(element)
+                    // setClips(clips => [...clips, element])
+                });                
+            })
+        });
+    })
+
     const styles = StyleSheet.create({
         container: {
             flex: 1,
