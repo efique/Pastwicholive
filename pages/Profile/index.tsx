1c7615e85ff77edba5937d52d50756e7d5d26e33
hochdyl <55688853+hochdyl@users.noreply.github.com>
2022-01-02 18:19:09 +0100

    Profile page update

--- pages/Profile/index.tsx
+++ pages/Profile/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import {Title} from "react-native-paper";
+import {ClipsList} from "../../components";
+import {getUsersData} from "../../utils/firebase";
 
 export default function ProfilePage() {
     const styles = StyleSheet.create({
@@ -11,12 +14,39 @@ export default function ProfilePage() {
             flexDirection: 'column',
             marginBottom: useBottomTabBarHeight()
         },
+        header: {
+            backgroundColor: 'rgb(190,174,255)',
+            padding: 10,
+            textAlign: "center"
+        }
     });
+
+    function getClips() {
+        let test = ['PopularAgileWoodpeckerShadyLulu', 'PopularAgileWoodpeckerShadyLulu', 'ScaryRelentlessVelociraptorBabyRage']
+        let clips = [];
+        test.forEach((id: string) => {
+            fetch(`https://api.twitch.tv/helix/clips?id=${id}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer jyjuqv7imq62o6gldfufqoqz4hklhr',
+                    'Client-id': 'zq9uuf6vb5lfmuhrvcu9vnodpq91pv'
+                }
+            })
+            .then((response) => response.json()) // liste des clips
+            .then((json) => {
+                json.data.forEach((element: any) => { // pour chaque clip
+                    clips.push(element)
+                });
+            })
+        });
+        return clips;
+    }
 
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView>
-                <Text>page</Text>
+                <Title style={styles.header}>VOS FAVORIS</Title>
+                <ClipsList clips={getClips()} />
             </ScrollView>
         </SafeAreaView>
     )
